32b4f1db47e7aeed2fb88a27f92ee013eca6bdc0
AntonRm <tworkn1@gmail.com>
2021-07-11 21:31:53 +0300

    start

---
+++ script.js
@@ -0,0 +1,83 @@
+// Тест 1
+// let a = 34;
+// let b = 20;
+// let c = 'Новая';
+
+// a = a + 2;
+// b = b - 5;
+// c = c + ' строка!';
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+//===========================================
+
+
+// Тест 2
+// let user = prompt('Введите сумму зарплаты:');
+// user = +user;
+// user = user + (user / 100 * 15);
+// console.log(user);
+// alert('Премия: 15%, итого: ' + user);
+// user = user - (user / 100 * 10);
+// console.log(user);
+// alert('Налоги: 10%, итого: ' + user);
+// user = user - 190;
+// console.log(user);
+// alert('В магазине: -190, итого: ' + user);
+// user = user / 2;
+// console.log(user);
+// alert('Отдал: половину, итого: ' + user);
+//===========================================
+
+
+// Тест 3
+// let pie = 17;
+// pie = --pie;
+// console.log('После кота: ' + pie);
+// pie = pie - 2;
+// console.log('После деда и внука: ' + pie);
+// pie = pie - 3;
+// console.log('После соседки: ' + pie);
+// pie = pie - 2;
+// console.log('После малышек: ' + pie);
+// pie = --pie;
+// console.log('После доярки: ' + pie);
+// pie = pie - 5;
+// console.log('После друзей: ' + pie);
+// pie = ++pie;
+// console.log('Один вернули: ' + pie);
+//===========================================
+
+
+// Тест 4
+// alert('Задумайте число!');
+// alert('Умножьте на 2.');
+// alert('Прибавьте к результату 7.');
+// let number = prompt('Введите что получилось:');
+// number = (number - 7) / 2;
+// alert('Вы задумали число: ' + number);
+// console.log(number);
+//===========================================
+
+
+// Тест 5
+// let person1 = prompt('Введите имя и фамилию студента: ');
+// let person2 = prompt('Введите имя и фамилию студента: ');
+// let person3 = prompt('Введите имя и фамилию студента: ');
+
+// document.write('<p>Параграф:</p><p>' + person1 + '<br>' + person2 + '<br>' + person3 + '</p>');
+// document.write('<p>Список:</p><ol><li>1. ' + person1 + '</li><li>2. ' + person2 + '</li><li>3. ' + person3 + '</li></ol>');
+
+
+// let person;
+// let result = '';
+// for (let i = 1; i <= 3; i++) {
+// 	person = prompt('Введите еще раз: ');
+// 	result = result + ('<li>' + i + '. ' + person + '</li>');
+// }
+// document.write('<p>Список циклом:</p><ol>' + result + '</ol>');
+//===========================================
+
+
+// Тест 6

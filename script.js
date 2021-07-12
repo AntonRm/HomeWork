46501f44321e96807aaa2daf088fbd5c44b9b7e1
ARm <tworkn1@gmail.com>
2021-07-12 20:30:01 +0300

    add new tasks

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Тест 1
+
 // let a = 34;
 // let b = 20;
 // let c = 'Новая';
@@ -14,6 +15,7 @@
 
 
 // Тест 2
+
 // let user = prompt('Введите сумму зарплаты:');
 // user = +user;
 // user = user + (user / 100 * 15);
@@ -32,6 +34,7 @@
 
 
 // Тест 3
+
 // let pie = 17;
 // pie = --pie;
 // console.log('После кота: ' + pie);
@@ -51,6 +54,7 @@
 
 
 // Тест 4
+
 // alert('Задумайте число!');
 // alert('Умножьте на 2.');
 // alert('Прибавьте к результату 7.');
@@ -62,6 +66,7 @@
 
 
 // Тест 5
+
 // let person1 = prompt('Введите имя и фамилию студента: ');
 // let person2 = prompt('Введите имя и фамилию студента: ');
 // let person3 = prompt('Введите имя и фамилию студента: ');
@@ -80,4 +85,366 @@
 //===========================================
 
 
+//=================================================================================================================================================
+// Условные конструкции if...else, switch ... case, тернарный оператор, операторы сравнения:
+//=================================================================================================================================================
+
+
+// Тест 1
+
+// let name = prompt('Введите имя:');
+// let yearBorn = prompt('Введите год рождения:');
+// let age = 2021 - +yearBorn;
+// document.write('<p>' + name + ': ' + age + '</p>');
+// console.log(name + ': ' + age);
+//===========================================
+
+
+// Тест 1.a
+
+// let name = prompt('Введите имя:');
+// let yearBorn = prompt('Введите год рождения:');
+// let age = 2021 - +yearBorn;
+// if (age > 10 && age <= 14) {
+// 	document.write('<p>' + name + ': ' + age + ' лет.</p>');
+// 	console.log(name + ': ' + age + ' лет.');
+// } else if ((age % 10) === 1) {
+// 	document.write('<p>' + name + ': ' + age + ' год.</p>');
+// 	console.log(name + ': ' + age + ' год.');
+// } else if ((age % 10) > 1 && (age % 10) <= 4) {
+// 	document.write('<p>' + name + ': ' + age + ' года.</p>');
+// 	console.log(name + ': ' + age + ' года.');
+// } else {
+// 	document.write('<p>' + name + ': ' + age + ' лет.</p>');
+// 	console.log(name + ': ' + age + ' лет.');
+// }
+//===========================================
+
+
+// Тест 1.b
+
+// let name = prompt('Введите имя:');
+// let yearBorn = prompt('Введите год рождения:');
+// let age = 2021 - +yearBorn;
+// (age > 10 && age <= 14) ? document.write('<p>' + name + ': ' + age + ' лет.</p>') :
+// 	((age % 10) === 1) ? document.write('<p>' + name + ': ' + age + ' год.</p>') :
+// 		((age % 10) > 1 && (age % 10) <= 4) ? document.write('<p>' + name + ': ' + age + ' года.</p>') :
+// 			document.write('<p>' + name + ': ' + age + ' лет.</p>');
+//===========================================
+
+
+// Тест 1.c
+
+// let name = prompt('Введите имя:');
+// let yearBorn = prompt('Введите год рождения:');
+// let currentDate = new Date();
+// let currentYear = currentDate.getFullYear();
+// let age = currentYear - +yearBorn;
+// (age > 10 && age <= 14) ? document.write('<p>' + name + ': ' + age + ' лет.</p>') :
+// 	((age % 10) === 1) ? document.write('<p>' + name + ': ' + age + ' год.</p>') :
+// 		((age % 10) > 1 && (age % 10) <= 4) ? document.write('<p>' + name + ': ' + age + ' года.</p>') :
+// 			document.write('<p>' + name + ': ' + age + ' лет.</p>');
+//===========================================
+
+
+// Тест 2
+
+// let floor = +prompt('Введите этаж:');
+// let prise = 0;
+// for (let i = 2; i <= floor; i++) {
+// 	if (i > 1 && i < 5) {
+// 		prise = prise + 20;
+// 	} else if (i > 4 && i < 8) {
+// 		prise = prise + 30;
+// 	} else {
+// 		prise = prise + 40;
+// 	}
+// }
+// document.write('<p>Заносим на ' + floor + ' этаж за ' + prise + ' грн.</p>');
+//===========================================
+
+
+// Тест 3
+
+// let userNumber = +prompt('Введите число от 1 до 10 включительно:');
+// let gameNumber = Math.floor(Math.random() * 10 + 1);
+// if (userNumber === gameNumber) {
+// 	document.write('<p>Вы угадали!</p>');
+// } else {
+// 	document.write('<p style="color:green">Не угадали! Ваше число ' + userNumber + ', а выпало число ' + gameNumber + '</p>');
+// }
+//===========================================
+
+
+// Тест 4
+
+// let userNumber = prompt('Введите четырехзначное число:');
+// let arr = userNumber.split('');
+// console.log(arr)
+// let result = 0;
+// if ((userNumber % 2) === 0) {
+// 	result = 1;
+// 	for (let i = 0; i < arr.length; i++) {
+// 		result = result * +arr[i];
+// 	}
+// 	document.write('<p>Число четное, произведение цифр: ' + result + '</p>');
+// } else {
+// 	for (let i = 0; i < arr.length; i++) {
+// 		result = result + +arr[i];
+// 	}
+// 	document.write('<p>Число нечетное, сумма цифр: ' + result + '</p>');
+// }
+//===========================================
+
+
+// Тест 5
+
+// let userNumber = +prompt('Введите число:');
+// if (isNaN(userNumber)) {
+// 	document.write('<p>Это не число!</p>');
+// }
+// let result = '';
+// for (let i = 2; i <= 10; i++) {
+// 	if (!(userNumber % i)) {
+// 		result = result + '<p>Число делится на ' + i + '</p><br>';
+// 	}
+// }
+// document.write(result);
+//===========================================
+
+
 // Тест 6
+
+// let userNumber = +prompt('Введите оклад:');
+// if (userNumber <= 6000) {
+// 	let tax = (userNumber / 100) * 10;
+// 	let salary = userNumber - tax;
+// 	document.write('<p>Налог: ' + tax + ', Оклад: ' + salary + '.</p>')
+// } else if (userNumber <= 10000) {
+// 	let tax = (userNumber / 100) * 15;
+// 	let salary = userNumber - tax;
+// 	document.write('<p>Налог: ' + tax + ', Оклад: ' + salary + '.</p>')
+// } else if (userNumber <= 15000) {
+// 	let tax = (userNumber / 100) * 17.5;
+// 	let salary = userNumber - tax;
+// 	document.write('<p>Налог: ' + tax + ', Оклад: ' + salary + '.</p>')
+// } else {
+// 	let tax = (userNumber / 100) * 20;
+// 	let salary = userNumber - tax;
+// 	document.write('<p>Налог: ' + tax + ', Оклад: ' + salary + '.</p>')
+// }
+//===========================================
+
+
+// Тест 7
+
+// let userBook;
+// let choice = '';
+// userBook = +confirm("Про волшебство?");
+// choice = choice + userBook;
+// userBook = +confirm("Чтобы было смешно?");
+// choice = choice + userBook;
+// userBook = +confirm("Детективная история?");
+// choice = choice + userBook;
+// userBook = +confirm("Ужастик?");
+// choice = choice + userBook;
+// if (choice === '0000') {
+// 	document.write('<p>Л.Н. Толстой. Война и мир</p>');
+// } else if (choice === '1000') {
+// 	document.write('<p>Джоан Роулинг.Гарри Поттер</p>');
+// } else if (choice === '1100') {
+// 	document.write('<p>Братья Стругацкие. Понедельник начинается в субботу</p>');
+// } else if (choice === '1110') {
+// 	document.write('<p>Агата Кристи. Вечеринка в Хэллоуин</p>');
+// } else if (choice === '1111') {
+// 	document.write('<p>Джонатан Страуд. Кричащая лестница</p>');
+// } else if (choice === '1010') {
+// 	document.write('<p>Диана Маш. Заноза для драконьего военачальника</p>');
+// } else if (choice === '0101') {
+// 	document.write('<p>Кирилл Круганский. Смешно или страшно</p><br><p>А. Маринина. Безупречная репутация</p>');
+// } else if (choice === '0001') {
+// 	document.write('<p>Стивен Кинг. Мизери</p>');
+// } else if (choice === '0011') {
+// 	document.write('<p>Эдгар По. Очень страшные истории</p>');
+// }
+//===========================================
+
+
+//=================================================================================================================================================
+// Циклы:
+//=================================================================================================================================================
+
+
+// Тест 1
+
+// let choice = confirm("Вывести изображения?");
+// if (choice) {
+// 	for (let i = 0; i < 2; i++) {
+// 		document.write('<img src="img/' + i + '.jpg" alt="">')
+// 	}
+// }
+//===========================================
+
+
+// Тест 2
+
+// let result = 0;
+// for (let i = 1; i <= 10; i++) {
+// 	result = result + i;
+// }
+// document.write('<p>Цикл for: ' + result + '</p>');
+// let result1 = 0;
+// let i = 1;
+// while (i <= 10) {
+// 	result1 = result1 + i;
+// 	i++;
+// }
+// document.write('<p>Цикл while: ' + result1 + '</p>');
+// let result2 = 0;
+// let j = 10;
+// do {
+// 	result2 = result2 + j;
+// 	j--;
+// } while (j >= 1);
+// document.write('<p>Цикл do while: ' + result2 + '</p>');
+//===========================================
+
+
+// Тест 3 (class: .style-1)
+
+// let width = 200;
+// for (let i = 1; i <= 5; i++) {
+// 	document.write('<p class="style-1" style="width: ' + width + 'px">Абзац' + i + '</p>');
+// 	width = width + 100;
+// }
+//===========================================
+
+
+// Тест 3 (class: .style-2)
+
+// for (let i = 1; i <= 5; i++) {
+// 	let a = Math.floor(Math.random() * 256);
+// 	let b = Math.floor(Math.random() * 256);
+// 	let c = Math.floor(Math.random() * 256);
+// 	document.write('<p class="style-2" style="background-color: rgb(' + a + ', ' + b + ', ' + c + ')">Абзац' + i + '</p>');
+// }
+//===========================================
+
+
+//================
+// Вложенные циклы
+//================
+
+
+// Тест 1
+
+
+//===========================================
+
+// Тест 2
+
+// let star = '';
+// for (let i = 1; i <= 7; i++) {
+// 	star = star + '*';
+// 	document.write('<p>' + star + '</p><br>');
+// }
+//===========================================
+
+
+// Тест 3
+
+// let row = '';
+// let column = '';
+// let counter = 1;
+// for (let i = 1; i <= 6; i++) {
+
+// 	for (let j = 1; j <= 4; j++) {
+// 		column = column + '<td>' + counter + '</td>';
+// 		counter++;
+// 	}
+// 	row = row + '<tr>' + column + '</tr>';
+// 	column = '';
+// }
+// document.write('<table>' + row + '</table>');
+//===========================================
+
+
+// Тест 4 (class: .style-table, .style-td, .style-td-black)
+
+// let row = '';
+// let column = '';
+// let counter = 1;
+// for (let i = 1; i <= 8; i++) {
+
+// 	for (let j = 1; j <= 8; j++) {
+// 		if (counter % 2) {
+// 			column = column + '<td class="style-td">\u00A0</td>';
+// 			counter++;
+// 		} else {
+// 			column = column + '<td class="style-td-black">\u00A0</td>';
+// 			counter++;
+// 		}
+// 	}
+// 	row = row + '<tr>' + column + '</tr>';
+// 	column = '';
+// 	counter++;
+// }
+// document.write('<table class="style-table">' + row + '</table>');
+//===========================================
+
+
+//=================================================================================================================================================
+// Массивы
+//=================================================================================================================================================
+
+
+// Тест 1a
+
+// const poem = ['Мороз', 'и', 'солнце', 'день', 'чудесный'];
+// let deleteNumber = +prompt('Введите номер слова, которое хотите заменить, от 1 до ' + poem.length);
+// while ((deleteNumber < 1) || (deleteNumber > poem.length)) {
+// 	deleteNumber = +prompt('Нужно ввести число от 1 до ' + poem.length);
+// }
+// let replaceWord = prompt('Введите слово, на которое хотите заменить:');
+// poem.splice(deleteNumber - 1, 1, replaceWord);
+// document.write('<p>' + poem.join(' ') + '</p>');
+//===========================================
+
+
+// Тест 1b
+
+// const poem = ['Мороз', 'и', 'солнце', 'день', 'чудесный'];
+// poem.splice(poem.length - 2, 1, 'пень');
+// document.write('<p>' + poem.join(' ') + '</p>');
+//===========================================
+
+
+// Тест 2
+// let arr = [];
+// for (let i = 0; i < 20; i++) {
+// 	arr[i] = Math.floor(Math.random() * 100 + 1);
+// }
+// let divisionTwo = arr.filter(function (item) {
+// 	if (item % 2 === 0) {
+// 		return true;
+// 	}
+// })
+// document.write('<p>' + divisionTwo + '</p>');
+// let divisionThree = arr.filter(function (item) {
+// 	if (item % 3 === 0) {
+// 		return true;
+// 	}
+// })
+// document.write('<p>' + divisionThree + '</p>');
+// let divisionFive = arr.filter(function (item) {
+// 	if (item % 5 === 0) {
+// 		return true;
+// 	}
+// })
+// document.write('<p>' + divisionFive + '</p>');
+// let divisionTen = arr.filter(function (item) {
+// 	if (item % 10 === 0) {
+// 		return true;
+// 	}
+// })
+// document.write('<p>' + divisionTen + '</p>');

6bd96f594441753b64e649d23cc5c7b6bb32a6e4
ARm <tworkn1@gmail.com>
2021-07-19 18:01:38 +0300

    123

--- 2/index.js
+++ 2/index.js
@@ -1,7 +1,11 @@
-// const modal = $.modal();
-// modal.open()
+const modal = $.modal({
+	title: 'Modal Window',
+	closable: true,
+	content: `
+		<h4>Modal is working</h4>
+		<p>qwrty qwrty qwerty qerty</p>
+		`,
+	width: '400px',
+});
+modal.open()
 
-const nextModal = $.modal();
-nextModal.open()
-nextModal.addTitle('Hello, mans!')
-setTimeout(nextModal.destroy, 5000)
